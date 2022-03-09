b84648ae8fc7aad1b00a6b5909d470ed0b181482
MD Rashid Hussain <mdrashid.hussain.786.01@gmail.com>
2022-03-09 22:32:55 +0530

    structure

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -1,51 +1,11 @@
 import React from "react";
-import { Button, Modal, ModalBody } from "reactstrap";
-import Select, { NonceProvider } from "react-select";
+import { Modal, ModalBody } from "reactstrap";
+import Select from "react-select";
 import { AiOutlineClose } from "react-icons/ai";
-
-import classes from "./login.module.css";
 import { toast } from "react-toastify";
 
-const loginUser = async (formData) => {
-  const response = await fetch("/api/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  });
-  const data = await response.json();
-  if (!response.ok) {
-    throw new Error(data.message || "Something went wrong");
-  }
-  return data;
-};
-
-const customSelectStyles = {
-  control: (base, state) => ({
-    ...base,
-    border: 0,
-    outline: 0,
-    borderBottom: "1px solid #ccc",
-    fontSize: "0.9rem",
-    marginTop: "10px",
-  }),
-  option: (base, state) => ({
-    ...base,
-    fontSize: "0.9rem",
-  }),
-  singleValue: (base, state) => ({
-    ...base,
-    fontSize: "0.9rem",
-  }),
-};
-
-const options = [
-  { value: "ADMIN", label: "ADMIN" },
-  { value: "DOCTOR", label: "DOCTOR" },
-  { value: "RECEPTIONIST", label: "RECEPTIONIST" },
-  { value: "PHARMACIST", label: "PHARMACIST" },
-];
+import { customSelectStyles, loginUser, options } from "./login.helpers";
+import classes from "./login.module.css";
 
 const Login = ({ toggleModal }) => {
   const [loading, setLoading] = React.useState(false);
