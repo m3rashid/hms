7a3b34433f26b0c0239c331160216ffa3059749a
MD Rashid Hussain <mdrashid.hussain.786.01@gmail.com>
2022-03-11 00:31:54 +0530

    auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { SessionProvider } from "next-auth/react";
 
 import Sidebar from "../components/Sidebar/Sidebar";
 import TopHeader from "../components/header/header";
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, session, pageProps }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <ToastContainer />
 
       <TopHeader />
@@ -18,7 +19,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }
 
